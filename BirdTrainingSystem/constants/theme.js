fbd533b0b44170e4592a21256f854e7d45a940f0
DThongLeon <hoangthong432001@gmail.com>
2023-09-26 16:34:05 +0700

    update new page

---
+++ BirdTrainingSystem/constants/theme.js
@@ -0,0 +1,25 @@
+const Colors =  {
+
+    primary: '#2A4D50',
+    secondary: '#DDF0FF',
+    tertiary: '#FF7754',
+
+    brownEa: '#EAC696',
+    brownC8: '#C8AE7D',
+    brown76: '#765827',
+    brown65: '#65451F',
+
+
+    white: '#FFFFFF',
+    lightWhite: '#FAFAFC',
+    offWhite: '#F3F4F8',
+
+    green: '#84FBB3',
+
+    black: '#000000',
+    
+    red: '#e81e4d',
+
+}
+
+export {Colors};
